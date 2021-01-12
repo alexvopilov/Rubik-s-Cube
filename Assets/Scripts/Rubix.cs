91faa86a7286db4542439bc5dec33c764d3d8739
Sasha <lonagi22@gmail.com>
2021-01-13 00:58:34 +0200

    Smooth rotation of side

--- Assets/Scripts/Rubix.cs
+++ Assets/Scripts/Rubix.cs
@@ -6,6 +6,19 @@ public class Rubix : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
+    IEnumerator Rotation(Transform v, Vector3 RotVector)
+    {
+        isRotating = true;
+        short _angle = 0;
+        while (_angle != 90)
+        {
+            _angle++;
+            v.RotateAround(TheCenter.transform.position,RotVector, 1);
+            yield return new WaitForSeconds(Time.deltaTime/5);
+        }
+        isRotating = false;
+        FindPositions(Cubies);
+    }
     {
         
     }
